4bb2f4c44899c55e25ab5dc97614a5d84a6595bd
David Daly <dalyd14@gmail.com>
2021-01-30 14:06:55 -0500

    finished adding relationshipd; first draft of model framework is set up; db seeded

--- models/index.js
+++ models/index.js
@@ -17,17 +17,29 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'tagged_product',
   foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'tagged_product',
   foreignKey: 'tag_id'
 })
 
+// ProductTag.belongsTo(Product, {
+//   foreignKey: 'product_id'
+// })
+// ProductTag.belongsTo(Tag, {
+//   foreignKey: 'tag_id'
+// })
+// Product.hasMany(ProductTag, {
+//   foreignKey: 'product_id'
+// })
+// Tag.hasMany(ProductTag, {
+//   foreignKey: 'tag_id'
+// })
 
 module.exports = {
   Product,
